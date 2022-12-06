fd9053bd9de3209779cff63ece6e699da9acd754
Richard Peres <richardperes.info@gmail.com>
2022-12-06 19:56:31 +0100

    Create the frontend UI that can run a full scenario of generation and solving

---
+++ frontend/README.md
@@ -0,0 +1,17 @@
+# Knapsac MVP UI
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+## How to use
+Simply click on __"Generate Knap Sac"__ then on __"Solve Knap Sac"__ buttons.
+> info: the current default endpoint for both backend services are `localhost:8080` and `localhost:8081`
---
+++ frontend/src/App.vue
@@ -0,0 +1,135 @@
+<script setup lang="ts">
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+import {ref} from "vue";
+
+let bagSize: number = 100
+let nbOfItems: number = 5
+
+let generatedKnapSac = ref(undefined)
+let displayGeneratedComp = ref(false)
+
+let solution = ref(undefined)
+let displaySolution = ref(false)
+
+function generatorUrl() {
+  return "http://localhost:8080/generate?bagSize=" + bagSize + "&numberOfItems=" + nbOfItems
+}
+
+function solveUrl() {
+  return "http://localhost:8081/solve"
+}
+
+async function generate() {
+  const response = await fetch(generatorUrl(), {
+    method: 'POST',
+    mode: "cors",
+    headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+  });
+
+  if (!response.ok) {
+    alert("error while generating")
+  }
+
+  generatedKnapSac.value = await response.json()
+  displayGeneratedComp.value = true
+  displaySolution.value = false
+
+}
+
+async function solve() {
+  const response = await fetch(solveUrl(), {
+    method: 'POST',
+    mode: "cors",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(generatedKnapSac.value)
+  });
+
+  if (!response.ok) {
+    alert("error while generating")
+  }
+
+  solution.value = await response.json()
+  displaySolution.value = true
+}
+</script>
+
+<template>
+  <div class="app">
+    <div class="form">
+      <div>
+        <p>Bag Size : {{ bagSize }}</p>
+        <input v-model.number="bagSize" placeholder="eg: 100"/>
+      </div>
+      <br>
+      <div>
+        <p>Number of items : {{ nbOfItems }}</p>
+        <input v-model.number="nbOfItems" placeholder="eg: 12"/>
+      </div>
+
+      <br><br>
+      <button @click="generate">Generate Knap Sac</button>
+
+      <br><br>
+      <button @click="solve">Solve Knap Sac</button>
+    </div>
+
+    <div class="sac" :class="{ 'display-none': !displayGeneratedComp }">
+      <h2>Generated Sac :</h2>
+      <br>
+      <vue-json-pretty :data=generatedKnapSac />
+    </div>
+
+    <div class="solution" :class="{ 'display-none': !displaySolution }">
+      <h2>Solution :</h2>
+      <br>
+      <vue-json-pretty :data=solution />
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+.app {
+  max-width: 70%;
+  width: fit-content;
+  margin: 10% auto 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.form {
+  display: inline-block;
+  margin-right: 50px;
+  padding-right: 50px;
+  border-right: 2px solid #2c3e50;
+}
+
+.sac {
+  display: inline-block;
+  max-height: 600px;
+  overflow-y: auto;
+  margin-right: 100px;
+  padding-right: 25px;
+  vertical-align: top;
+}
+
+.solution {
+  display: inline-block;
+  max-height: 600px;
+  overflow-y: auto;
+  margin-right: 50px;
+  padding-right: 25px;
+  vertical-align: top;
+}
+
+.display-none {
+  display: none;
+}
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: darkgrey;
+  opacity: 1; /* Firefox */
+}
+</style>
